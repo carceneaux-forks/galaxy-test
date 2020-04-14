8d08806f6211738bc2c933115f20723c55f261a3
Chris Arceneaux <carcenea@gmail.com>
2020-04-14 18:01:13 -0400

    test
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#ansible-galaxy collection build
-#ansible-galaxy collection publish ./$COLLECTION_NAMESPACE-$COLLECTION_NAME-$VERSION.tar.gz --api-key $ANSIBLE_GALAXY_TOKEN
-echo $COLLECTION_NAMESPACE-$COLLECTION_NAME-$VERSION.tar.gz
